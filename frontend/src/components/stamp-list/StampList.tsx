974f2999364e886dba39c74f2e94aea0cc6c5659
gwisp2 <60121533+gwisp2@users.noreply.github.com>
2021-03-26 19:52:25 +0300

    Infinite scrolling for stamp list

--- frontend/src/components/stamp-list/StampList.tsx
+++ frontend/src/components/stamp-list/StampList.tsx
@@ -1,22 +1,63 @@
 import React from "react";
 import {Stamp} from "../../model/stamps";
 import './StampList.css'
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export interface StampListProps {
     stamps: Array<Stamp>
 }
 
-export class StampList extends React.Component<StampListProps, {}> {
+export interface StampListState {
+    allItems: Array<Stamp>
+    shownItems: Array<Stamp>
+}
+
+export class StampList extends React.Component<StampListProps, StampListState> {
+    static BatchSize = 50
+
+    constructor(props: StampListProps) {
+        super(props);
+        console.log(this.props.stamps)
+        this.showMoreItems = this.showMoreItems.bind(this);
+    }
+
+    static getDerivedStateFromProps(props: StampListProps, currentState: StampListState|null) {
+        if (currentState === null || props.stamps !== currentState.allItems) {
+            return {
+                allItems: props.stamps,
+                shownItems: props.stamps.slice(0, StampList.BatchSize)
+            }
+        } else {
+            return null
+        }
+    }
+
+    private showMoreItems() {
+        const startIndex = this.state.shownItems.length
+        const endIndex = Math.min(this.props.stamps.length, this.state.shownItems.length + StampList.BatchSize);
+        this.setState({
+            shownItems: this.state.shownItems.concat(this.props.stamps.slice(startIndex, endIndex))
+        })
+    }
+
     render() {
-        return (<div className="stamp-list">
-            {this.props.stamps.map((s) => {
-                return (<div key={s.id} className="stamp-card">
-                    <div className="stamp-card-id">№ {s.id} [{s.value}₽] // {s.year}</div>
-                    <div>
-                        <img draggable="false" alt={"Image of stamp " + s.id} className="stamp-image" src={(s.imageUrl ?? new URL("empty.png", document.baseURI)).toString()}/>
-                    </div>
-                </div>)
-            })}
-        </div>);
+        return (<InfiniteScroll
+            hasMore={this.state.shownItems.length !== this.props.stamps.length}
+            dataLength={this.state.shownItems.length}
+            loader={<h4>Загрузка...</h4>}
+            next={this.showMoreItems}
+        >
+            <div className="stamp-list">
+                {this.state.shownItems.map((s) => {
+                    return (<div key={s.id} className="stamp-card">
+                        <div className="stamp-card-id">№ {s.id} [{s.value}₽] // {s.year}</div>
+                        <div>
+                            <img draggable="false" alt={"Image of stamp " + s.id} className="stamp-image"
+                                 src={(s.imageUrl ?? new URL("empty.png", document.baseURI)).toString()}/>
+                        </div>
+                    </div>)
+                })}
+            </div>
+        </InfiniteScroll>);
     }
 }
