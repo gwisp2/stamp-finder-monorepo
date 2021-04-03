ff9d35ea6e1a4979c28dde20124afa72f2ce92ff
gwisp2 <60121533+gwisp2@users.noreply.github.com>
2021-04-03 21:54:07 +0300

    Show stamp id on top, change margins in a stamp card

--- frontend/src/components/stamp-card/StampCard.tsx
+++ frontend/src/components/stamp-card/StampCard.tsx
@@ -11,12 +11,12 @@ export class StampCard extends React.Component<Props, {}> {
     render() {
         const s = this.props.stamp;
         return (<div className="position-relative shadow-sm bg-light border border-secondary rounded p-2">
-            <div className="stamp-card-image-container">
+            <div className="stamp-card-id mb-1">№ {s.id} [{s.value}₽] // {s.year}</div>
+            <div className="stamp-card-image-container mb-1">
                 <div className="stamp-card-image-container-dummy"/>
                 <img draggable="false" alt={"Image of stamp " + s.id} className="stamp-image"
                      src={(s.imageUrl ?? EmptyImage).toString()}/>
             </div>
-            <div className="stamp-card-id">№ {s.id} [{s.value}₽] // {s.year}</div>
             <a className={"w-100 btn " + (s.present ? "btn-success" : "btn-secondary")} href={s.page.toString()} target="_blank" rel="noreferrer">{s.present ? "Купить" : "Нет в наличии"}</a>
         </div>);
     }
