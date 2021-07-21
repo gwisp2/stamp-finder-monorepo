4d12da894487ecbcf28d015184bb6509999c3d84
gwisp2 <60121533+gwisp2@users.noreply.github.com>
2021-07-21 12:53:11 +0300

    Send page view events on change of search options

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,9 +4,15 @@ import App from './App';
 import './index.css';
 import {BrowserRouter, withRouter} from "react-router-dom";
 
-const AppWithRouter = withRouter(({history}) => (
-    <App history={history}/>
-))
+const AppWithRouter = withRouter(({history}) => {
+    history.listen((location) => {
+        if (typeof window.ga === 'function') {
+            window.ga('set', 'page', location.pathname + location.search);
+            window.ga('send', 'pageview');
+        }
+    });
+    return <App history={history}/>
+})
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
