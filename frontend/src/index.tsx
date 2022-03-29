42b2a585bf771d4c4ecde0651982472e85877649
gwisp2 <60121533+gwisp2@users.noreply.github.com>
2022-03-29 18:02:45 +0300

    Don't hardcode google analytics token

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,15 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import ReactGA from 'react-ga';
 import { BrowserRouter, withRouter } from 'react-router-dom';
 
+const ga_id = process.env.REACT_APP_GA_ID;
+const ga_is_enabled = ga_id !== undefined && ga_id.length !== 0;
+if (ga_id !== undefined && ga_is_enabled) {
+  console.log(`GA ID is ${ga_id}`);
+  ReactGA.initialize(ga_id);
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
 const AppWithRouter = withRouter(({ history }) => {
-  history.listen((location) => {
-    if (typeof window.ga === 'function') {
-      window.ga('set', 'page', location.pathname + location.search);
-      window.ga('send', 'pageview');
-    }
-  });
+  if (ga_is_enabled) {
+    history.listen(() => {
+      ReactGA.pageview(window.location.pathname + window.location.search);
+    });
+  }
   return <App history={history} />;
 });
 ReactDOM.render(
