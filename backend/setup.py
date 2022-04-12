27af56fddfebf2f1f20471d4dd5518e2904b972f
gwisp2 <60121533+gwisp2@users.noreply.github.com>
2022-04-12 12:57:36 +0300

    Create Dockerfile

--- backend/setup.py
+++ backend/setup.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
 from setuptools import find_packages, setup
 
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines() if len(i.strip()) != 0]
+
 setup(
     name="stamp-finder-backend",
     version="0.0.1",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=[
-        "pydantic",
-        "pyyaml",
-        "docopt",
-        "loguru",
-        "stamp-finder-scripts @ git+https://github.com/gwisp2/stamp-finder-scripts.git#4ab8b66afe6f09bf628d3ccb52803c799ecb7b34"
-    ],
+    install_requires=REQUIREMENTS,
     extras_require={
         "dev": [
             "black",
