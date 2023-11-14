aa8fb9fcc5e09e86ce0e95f559ea544f1249662a
gwisp2 <60121533+gwisp2@users.noreply.github.com>
2023-11-14 15:28:37 +0300

    Add 'about' page

---
+++ frontend/src/pages/about.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import InfoIcon from '@mui/icons-material/Info';
+import {Page} from "./page.ts";
+import {Box} from "@mui/material";
+import Button from "@mui/material/Button";
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+function AboutPageContent() {
+    const githubLink: string = import.meta.env.VITE_SF_GITHUB_LINK
+    return <Box width="100%" p={2}>
+        <Box display="flex" mb={2}>
+            <Button variant="contained" color="secondary" endIcon={<GitHubIcon/>} target="_blank"
+                    href={githubLink}>Github</Button>
+        </Box>
+        <Box>Здесь можно было написать что-нибудь, но зачем?</Box>
+    </Box>
+}
+
+export const About: Page = {
+    key: 'about',
+    navIcon: <InfoIcon/>,
+    navText: 'О сайте',
+    renderContent(): React.ReactNode {
+        return <AboutPageContent/>;
+    },
+    renderSidebar(): React.ReactNode {
+        return <></>;
+    },
+};
--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,4 +1,5 @@
 import { Search } from './search.tsx';
 import { Favorites } from './favorites.tsx';
+import {About} from "./about.tsx";
 
-export default [Search, Favorites];
+export default [Search, Favorites, About];
